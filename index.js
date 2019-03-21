3c96fd1e06e0954682067b0d6be5bbd8c217ae09
agohorel <alex.oddlogic@gmail.com>
2019-03-21 16:18:06 -0400

    getting there...

--- index.js
+++ index.js
@@ -46,18 +46,28 @@ let asciiArt = `
 `;
 
 const canvas = document.querySelector(".ascii");
-let index = 0;
+
+let indexes = {
+    idx1: 0,
+    idx2: 0,
+    idx3: 0
+};
+
 canvas.innerText = asciiArt;
 
 setInterval( ()=> {
+    console.log(indexes);
+    swapChars(indexes, "idx1", "*");
+    swapChars(indexes, "idx2", "@");
+    // swapChars(indexes, "idx3", ")");
+}, 250);
 
-    swapChars("*");
-
-    // // skip whitespace
-    // if (/\S/.test(asciiArt[index])) {
-    //     canvas.innerText = replaceAt(asciiArt, index, randomChar());
-    // } 
-}, 50);
+function swapChars(indexes, idx, target) {
+    let locs = getLocs(asciiArt, target);
+    canvas.innerText = replaceAt(asciiArt, locs[indexes[idx]], randomChar());
+    indexes[idx]++;
+    if (indexes[idx] > locs.length-1) indexes[idx] = 0;
+}
 
 function getLocs(string, target){
     let arr = [];
@@ -69,18 +79,11 @@ function getLocs(string, target){
     return arr;
 }
 
-function swapChars(target){
-    let locs = getLocs(asciiArt, target);
-    canvas.innerText = replaceAt(asciiArt, locs[index], randomChar());
-    index++;
-    if (index > locs.length - 1) index = 0;
+function replaceAt(string, index, replace) {
+    return string.substring(0, index) + replace + string.substring(index + 1);
 }
 
 function randomChar(){
     let chars = ["!", "@", "#", "$", "%", "^", "*", "+", "=", "_"];
     return chars[Math.floor(Math.random() * chars.length)];
-}
-
-function replaceAt(string, index, replace) {
-    return string.substring(0, index) + replace + string.substring(index + 1);    
 }
