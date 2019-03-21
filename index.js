b3b71fbeadf034c095c2e142b8b60f0f7bbb70c8
agohorel <alex.oddlogic@gmail.com>
2019-03-21 19:10:49 -0400

    new approach

--- index.js
+++ index.js
@@ -1,89 +1,10 @@
-let asciiArt = `
-          *                                             *
-                                               *
-                    *
-                                  *
-                                                            *
-         *
-                                                  *
-             *
-                           *             *
-                                                     *
-      *                                                               *
-               *
-                               (             )
-                       )      (*)           (*)      (
-              *       (*)      |             |      (*)
-                       |      |~|           |~|      |          *
-                      |~|     | |           | |     |~|
-                      | |     | |           | |     | |
-                     ,| |a@@@@| |@@@@@@@@@@@| |@@@@a| |.
-                .,a@@@| |@@@@@| |@@@@@@@@@@@| |@@@@@| |@@@@a,.
-              ,a@@@@@@| |@@@@@@@@@@@@.@@@@@@@@@@@@@@| |@@@@@@@a,
-             a@@@@@@@@@@@@@@@@@@@@@' . \`@@@@@@@@@@@@@@@@@@@@@@@@a
-             ;\`@@@@@@@@@@@@@@@@@@'   .   \`@@@@@@@@@@@@@@@@@@@@@';
-             ;@@@\`@@@@@@@@@@@@@'     .     \`@@@@@@@@@@@@@@@@'@@@;
-             ;@@@;,.aaaaaaaaaa       .       aaaaa,,aaaaaaa,;@@@;
-             ;;@;;;;@@@@@@@@;@      @.@      ;@@@;;;@@@@@@;;;;@@;
-             ;;;;;;;@@@@;@@;;@    @@ . @@    ;;@;;;;@@;@@@;;;;;;;
-             ;;;;;;;;@@;;;;;;;  @@   .   @@  ;;;;;;;;;;;@@;;;;@;;
-             ;;;;;;;;;;;;;;;;;@@     .     @@;;;;;;;;;;;;;;;;@@@;
-         ,%%%;;;;;;;;@;;;;;;;;       .       ;;;;;;;;;;;;;;;;@@;;%%%,
-      .%%%%%%;;;;;;;@@;;;;;;;;     ,%%%,     ;;;;;;;;;;;;;;;;;;;;%%%%%%,
-     .%%%%%%%;;;;;;;@@;;;;;;;;   ,%%%%%%%,   ;;;;;;;;;;;;;;;;;;;;%%%%%%%,
-     %%%%%%%%\`;;;;;;;;;;;;;;;;  %%%%%%%%%%%  ;;;;;;;;;;;;;;;;;;;'%%%%%%%%
-     %%%%%%%%%%%%\`;;;;;;;;;;;;,%%%%%%%%%%%%%,;;;;;;;;;;;;;;;'%%%%%%%%%%%%
-     \`%%%%%%%%%%%%%%%%%,,,,,,,%%%%%%%%%%%%%%%,,,,,,,%%%%%%%%%%%%%%%%%%%%'
-       \`%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-           \`%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-                  """"""""""""""\`,,,,,,,,,'"""""""""""""""""
-                                 \`%%%%%%%'
-                                  \`%%%%%'
-                                    %%%     
-                                   %%%%%
-                                .,%%%%%%%,.
-                           ,%%%%%%%%%%%%%%%%%%%,
-`;
-
 const canvas = document.querySelector(".ascii");
-
-let indexes = {
-    idx1: 0,
-    idx2: 0,
-    idx3: 0
-};
-
 canvas.innerText = asciiArt;
 
+let iterator = 0;
+
 setInterval( ()=> {
-    console.log(indexes);
-    swapChars(indexes, "idx1", "*");
-    swapChars(indexes, "idx2", "@");
-    // swapChars(indexes, "idx3", ")");
-}, 250);
-
-function swapChars(indexes, idx, target) {
-    let locs = getLocs(asciiArt, target);
-    canvas.innerText = replaceAt(asciiArt, locs[indexes[idx]], randomChar());
-    indexes[idx]++;
-    if (indexes[idx] > locs.length-1) indexes[idx] = 0;
-}
-
-function getLocs(string, target){
-    let arr = [];
-    for (let i = 0; i < string.length; i++){
-        if (string[i] === target){
-            arr.push(i);
-        }
-    }
-    return arr;
-}
-
-function replaceAt(string, index, replace) {
-    return string.substring(0, index) + replace + string.substring(index + 1);
-}
-
-function randomChar(){
-    let chars = ["!", "@", "#", "$", "%", "^", "*", "+", "=", "_"];
-    return chars[Math.floor(Math.random() * chars.length)];
-}
+    canvas.innerText = asciis[iterator];
+    iterator++;
+    iterator > asciis.length - 1 ? iterator = 0 : iterator = iterator;
+}, 125);
